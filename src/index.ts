c0f0f90715dff454159801d5c9681d08771aabc6
Patrick McClurg <patrick.mcclurg@protocol.ai>
2023-02-08 11:35:45 +0100

    updated to fetch default export

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,12 @@
-addEventListener("fetch", event => {
-    handleRequest(event.request).then(response => event.respondWith(response))
-})
+export default {
+    async fetch(request: Request): Promise<Response> {
+        const response = await fetch(request)
+        const copy = new Response(response.body, response)
 
-async function handleRequest(request: Request): Promise<Response> {
-    const response = await fetch(request)
-    const copy = new Response(response.body, response)
+        copy.headers.append("Strict-Transport-Security", "max-age=31536000; includeSubDomains")
+        copy.headers.append("X-Frame-Options", "DENY")
+        copy.headers.append("Permissions-Policy", "geolocation=(), camera=(), accelerometer=(), camera=(), display-capture=(), gyroscope=(), microphone=(), magnetometer=(), payment=(), usb=(), xr-spatial-tracking=()")
 
-    copy.headers.append("Strict-Transport-Security", "max-age=31536000; includeSubDomains")
-	copy.headers.append("X-Frame-Options", "DENY")
-	copy.headers.append("Permissions-Policy", "geolocation=(), camera=(), accelerometer=(), camera=(), display-capture=(), gyroscope=(), microphone=(), magnetometer=(), payment=(), usb=(), xr-spatial-tracking=()")
-
-	return copy
-}
+        return copy
+    },
+};
