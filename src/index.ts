3f5981d99efae9f4e31d72883bbbbed0e4ccace5
Patrick McClurg <patrick.mcclurg@protocol.ai>
2023-03-28 03:59:55 +0200

    added a CSP here instead of as a <meta> tag

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ export default {
         copy.headers.append("Strict-Transport-Security", "max-age=31536000; includeSubDomains")
         copy.headers.append("X-Frame-Options", "DENY")
         copy.headers.append("Permissions-Policy", "geolocation=(), camera=(), accelerometer=(), camera=(), display-capture=(), gyroscope=(), microphone=(), magnetometer=(), payment=(), usb=(), xr-spatial-tracking=()")
+        copy.headers.append("Content-Security-Policy", "default-src 'self' api.drand.sh; img-src 'self';")
 
         return copy
     },
